ea7ac507c699fb439d80cbcfb393d9c68260fbd3
Claudio Reis <cl4ud1or31s@gmail.com>
2024-07-14 19:41:05 -0300

    cadastro com validação

--- setup/settings.py
+++ setup/settings.py
@@ -151,3 +151,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Mensagens de Erro Bootstrap ALERT
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User #importando tabela Usuario do django
+
+#para alerta de mensagens de erro
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
 def cadastro(request):
@@ -11,10 +16,33 @@ def cadastro(request):
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
         
-       
         if senha != confirmar_senha:
-            print('Erro 2')
+            messages.add_message(request, constants.ERROR, "A senha e confirmar senha devem ser iguais")
             return redirect('/usuarios/cadastro')
-        return HttpResponse(f'{username}--{email}--{senha}--{confirmar_senha}')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, "A senha deve ter mais de 6 digitos")
+            return redirect('/usuarios/cadastro')
+
+        users = User.objects.filter(username=username)
+         
+        #se o usuario ja existe redireciona 
+        if users.exists():
+            messages.add_message(request, constants.ERROR, "Já existe um usuário com esse username")
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=senha
+            )
+            return redirect('/usuarios/login')
+        except:
+            print('Erro 4')
+            return redirect('/usuarios/cadastro')
+        
+        
+       
         
 
